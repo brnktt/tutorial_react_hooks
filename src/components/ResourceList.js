71626c69e056c1fd1c6f8466845bd6a5963191e8
Branchetti Alessandro <40342541+brnktt@users.noreply.github.com>
2020-04-28 18:20:00 +0200

    replaced statefull components with stateless using useState&useEffect hooks

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,34 +1,28 @@
-import React from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-class ResourceList extends React.Component {
-  state = {
-    resources: [],
-  }
+const ResourceList = ({ resourceName }) => {
+  const [resources, setResources] = useState([]);
 
-  async componentDidMount() {
-    const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resourceName}`);
+  const fetchResources = async (resourceName) => {
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/${resourceName}`
+    );
 
-    this.setState({ resources: response.data });
-  }
+    setResources(response.data);
+  };
 
-  async componentDidUpdate(prevProps) {
-    if(prevProps.resourceName !== this.props.resourceName) {
-      const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resourceName}`);
+  useEffect(() => {
+    fetchResources(resourceName);
+  }, [resourceName]);
 
-      this.setState({ resources: response.data });
-    }
-  }
-
-  render() {
-    return (
-      <ul>
-        {this.state.resources.map(resource => (
-          <li key={resource.id}>{resource.title}</li>
-        ))}
-      </ul>
-    )
-  }
-}
+  return (
+    <ul>
+      {resources.map((resource) => (
+        <li key={resource.id}>{resource.title}</li>
+      ))}
+    </ul>
+  );
+};
 
 export default ResourceList;
